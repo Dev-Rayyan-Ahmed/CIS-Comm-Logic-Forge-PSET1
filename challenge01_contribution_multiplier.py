8fc20581cede84c205d6cc80eb380a7f1061b35e
DevRayyan <rayyanahmed1624@gmail.com>
2025-12-31 03:11:28 +0500

    Update: Prefix and Suffix Approach

--- challenge01_contribution_multiplier.py
+++ challenge01_contribution_multiplier.py
@@ -1,14 +1,28 @@
 def calculate_impact(contributions:list):
-    impacts = []
-    for contribute in contributions:
-        impact = 1
-        for score in contributions:
-            if score == contribute:
-                continue
-            impact = impact*score
-        impacts.append(impact)
+    n = len(contributions)
+    impacts = [1]*n
+
+    #calculating left product:
+    left_product = 1
+    for i in range(n):
+        impacts[i] = left_product
+        left_product *= contributions[i]
+    
+    #calculating right product:
+    right_product = 1
+    for i in range(-1, -(n+1), -1):
+        #using negative indexing here
+        impacts[i] *= right_product
+        right_product *= contributions[i]
+    
     return impacts
         
 test_contributions = [1,2,3,4]
-# impacts = [24,12,8,6]
+# impacts should be = [24,12,8,6]
 print(calculate_impact(test_contributions))
+
+# testing
+# n = 4
+# for i in range(-1,-(n+1),-1):
+#     print(test_contributions[i])
+    # print(i)
