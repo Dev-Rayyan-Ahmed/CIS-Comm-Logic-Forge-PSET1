96ffd9038261f783babe8698f6d599baadb36c0e
DevRayyan <rayyanahmed1624@gmail.com>
2025-12-31 03:19:28 +0500

    Update: Optimized Approach

--- challenge01_contribution_multiplier.py
+++ challenge01_contribution_multiplier.py
@@ -1,28 +1,25 @@
-def calculate_impact(contributions:list):
-    n = len(contributions)
-    impacts = [1]*n
+def calculate_impact(contributions: list)-> list:
 
-    #calculating left product:
+    n = len(contributions)
+    if n <= 0:
+        return []
+
+    impacts = [1] * n
+
+    # calculating left product:
     left_product = 1
     for i in range(n):
         impacts[i] = left_product
         left_product *= contributions[i]
-    
-    #calculating right product:
+
+    # calculating right product:
     right_product = 1
-    for i in range(-1, -(n+1), -1):
-        #using negative indexing here
+    for i in range(n - 1, -1, -1):
         impacts[i] *= right_product
         right_product *= contributions[i]
-    
+
     return impacts
-        
-test_contributions = [1,2,3,4]
+
+test_contributions = [1, 2, 3, 4]
 # impacts should be = [24,12,8,6]
 print(calculate_impact(test_contributions))
-
-# testing
-# n = 4
-# for i in range(-1,-(n+1),-1):
-#     print(test_contributions[i])
-    # print(i)
